d21e1d115082c561f8076592452a5651f7790497
miguel.reuter@ecl14.ec-lyon.fr <miguel.reuter@ecl14.ec-lyon.fr>
2018-03-31 16:38:24 +0200

    cross entropy loss + ReLU + Batch noralization

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 # encoding : utf-8
 
 import numpy as np
+import matplotlib.pyplot as plt
+from mnist import MNIST
+
 from neural_network import *
+
+
+def lecture_mnist(data_path):
+	mdata = MNIST(data_path)
+	data_app, label_app = mdata.load_training()
+	data_test, label_test = mdata.load_testing()
+
+	return np.array(data_app), np.array(data_test), np.array(label_app), np.array(label_test)
+
+def pick_batches(n, data, label):
+	_xr = np.arange(data.shape[0])
+	np.random.shuffle(_xr)
+	idx = _xr[:n]
+
+	x = data[idx, :]
+	y_cl = label[idx]
+
+	y = np.zeros((n, 10))
+	for i in range(n):
+		y[i, y_cl[i]] = 1
+
+	x = preprocess_data(x)
+
+	return x, y, y_cl
+
+def preprocess_data(x):
+	return (x.T - np.mean(x, axis=1)).T
 
 
 
@@ -15,17 +45,88 @@ if __name__ == "__main__":
 	# D_in est la dimension des données d'entrée
 	# D_h le nombre de neurones de la couche cachée
 	# D_out est la dimension de sortie (nombre de neurones de la couche de sort
+	
+	"""
 	N, D_in, D_h, D_out = 30, 2, 10, 3
 	learning_rate = 0.1
+
+	#custom_sig = AF_ReLU()
+	custom_sig = AF_Sigmoid()
+	mse = L_MSE()
+	#mse = L_SoftMax()
 	
+	losses = []
+
 	# Création d'une matrice d'entrée X et de sortie Y avec des valeurs aléatoires
 	X = np.random.random((N, D_in))
 	Y = np.random.random((N, D_out))
 
-	nn = Network([D_in, D_h, D_out], AF_Sigmoid, L_MSE, learning_rate)
-	for i in range(30000):
+	nn = Network([D_in, D_h, D_out], custom_sig, mse, learning_rate)
+	for i in range(epochs_max):
 		Ypred = nn.forward_propagation(X)
 		loss = nn.compute_loss(Ypred, Y)
+		losses.append(loss)
 		if i % 1000 == 0:		
 			print("iteration {} : ".format(i), loss)
 		nn.back_propagation(Ypred, Y)
+	"""
+	data_app, data_test, label_app, label_test	= lecture_mnist("../mnist-data")
+
+	N = 64
+	epochs_max = 300
+	
+	# N (28*28) px images - 10 classes
+	D_in, D_out = 28*28, 10
+
+	hidden_layers_dim = [50]
+	learning_rate = 0.00005
+
+	af_func = AF_ReLU()
+	af_func = AF_Sigmoid()
+	#loss_func = L_MSE()
+	loss_func = L_SoftMax()
+
+	nn = Network([D_in, *hidden_layers_dim, D_out], af_func, loss_func, learning_rate, batch_normalization=True)
+	
+	# train
+	losses = []
+	for epoch in range(epochs_max):
+		X, Y, Y_label = pick_batches(N, data_app, label_app)
+
+		Ypred = nn.forward_propagation(X)
+		loss = nn.compute_loss(Ypred, Y)
+		nn.back_propagation(Ypred, Y)
+		
+		losses.append(loss)
+	
+	# test
+	X, Y, Y_label = pick_batches(N, data_test, label_test)
+	Ypred = nn.forward_propagation(X)
+	y_pred_cl = np.argmax(Ypred, axis=1)
+	acc = np.mean(np.array(y_pred_cl == Y_label, dtype=int))
+	print("acc = ", acc)
+
+	# plot loss(epoch)
+	fig = plt.figure()	
+	plt.plot(losses)
+	plt.axis([0, epochs_max, 0, max(losses) + 1])
+	plt.draw()
+	plt.pause(1)
+	print(loss)
+	input("PRESS ANY KEY TO CONTINUE.")
+	plt.close(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
--- neural_network.py
+++ neural_network.py
@@ -4,46 +4,68 @@ import numpy as np
 
 # Loss functions
 class LossFunction:
-	def apply(Ypred, Y):
+	def apply(self, Ypred, Y):
 		print("abstract method")
-	def apply_derivative(Ypred, Y):
+	def apply_derivative(self, Ypred, Y):
 		print("abstract method")
+
 
 class L_MSE(LossFunction):
-	def apply(Ypred, Y):
+	def apply(self, Ypred, Y):
 		return np.square(Y - Ypred).sum() / 2
-	def apply_derivative(Ypred, Y):
-		return Y - Ypred
+	
+	def apply_derivative(self, Ypred, Y):		
+		return Ypred - Y
 
+
+class L_SoftMax(LossFunction):
+	def apply(self, Ypred, Y):
+		e = np.exp( np.max(Ypred*Y, axis=1)) / np.sum(np.exp(Ypred), axis=1)
+		L = np.mean(-np.log(e))
+		return L
+
+	def apply_derivative(self, Ypred, Y):
+		e = np.exp(Ypred*Y) / np.sum(np.exp(Ypred), axis=0)
+		return e - Y
 
 
 # Activation functions
 class ActivationFunction:
-	def apply(I):
+	def apply(self, I):
 		print("abstract method")
 	
-	def apply_derivative(loss):
+	def apply_derivative(self, loss):
 		print("abstract method")
 
 
 class AF_Sigmoid(ActivationFunction):
-	def apply(I):
+	def apply(self, I):
 		return 1 / (1 + np.exp(-I))
 	
-	def apply_derivative(output):
+	def apply_derivative(self, output):
 		return output * (1 - output)
 
+
+class AF_ReLU(ActivationFunction):
+	def apply(self, I):
+		return np.maximum(0, I)
+	
+	def apply_derivative(self, output):
+		d = np.array(output > 0, dtype=float)
+		return d
 
 
 
 # Neural Network
 class Layer:
-	def __init__(self, input_size, output_size, activation_func, learning_rate):
+	def __init__(self, input_size, output_size, activation_func, learning_rate, batch_normalization=False):
 		self.input_size = input_size
 		self.ouptut_size = output_size
 		
 		self.activation_func = activation_func
 		self.learning_rate = learning_rate
+
+		self.batch_normalization = batch_normalization
 		# weights and biases
 		self.O = None
 		self.I = None
@@ -53,21 +75,27 @@ class Layer:
 	def forward_propagation(self, I):
 		self.I = I
 		O = I.dot(self.W) + self.B
+		# normalization
+		if self.batch_normalization:
+			O = (O - np.mean(O, axis=0)) / np.sqrt(np.var(O, axis=0))
 		self.O = self.activation_func.apply(O)
+		
 		return self.O
 
 	def back_propagation(self, loss):
 		d = loss * self.activation_func.apply_derivative(self.O)
-		self.update_weights(self.learning_rate, d)
+		self.update_weights(d)
 		return d.dot(self.W.T)
 	
-	def update_weights(self, learning_rate, delta):
-		self.W += learning_rate * self.I.T.dot(delta)		
+	def update_weights(self, delta):
+		self.W -= self.learning_rate * self.I.T.dot(delta)
+		self.B = self.B - self.learning_rate * delta
 
 class Network:
-	def __init__(self, layers_dim, activation_func, loss_func, learning_rate):
+	def __init__(self, layers_dim, activation_func, loss_func, learning_rate, batch_normalization=False):
 		self.activation_func = activation_func
 		self.learning_rate = learning_rate
+		self.batch_normalization = batch_normalization
 		self.loss_func = loss_func
 		self.layers = []
 		
@@ -75,7 +103,7 @@ class Network:
 
 	def create_layers(self, layers_dim):
 		for i in range(len(layers_dim) - 1):
-			self.layers.append(Layer(layers_dim[i], layers_dim[i+1], self.activation_func, self.learning_rate))
+			self.layers.append(Layer(layers_dim[i], layers_dim[i+1], self.activation_func, self.learning_rate, self.batch_normalization))
 
 	def add_layer(self, layer):
 		self.layers.append(layer)
@@ -87,7 +115,6 @@ class Network:
 		return Ypred
 
 	def back_propagation(self, Ypred, Y):
-
 		curr_grad = self.loss_func.apply_derivative(Ypred, Y)
 
 
@@ -100,3 +127,4 @@ class Network:
 
 	def compute_loss(self, Ypred, Y):
 		return self.loss_func.apply(Ypred, Y)
+
